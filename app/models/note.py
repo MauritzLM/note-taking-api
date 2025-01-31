c0edbf61f1503062ac315b6cb87f4c70b67db787
Mauritz Marais <mauritz303@gmail.com>
2025-01-31 16:48:56 +0200

    updated id fields type

--- app/models/note.py
+++ app/models/note.py
@@ -1,13 +1,14 @@
 # note model
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from app.core.config import Base
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class note(Base):
     __tablename__ = 'notes'
 
-    id: uuid.UUID = Column(default=uuid.uuid4, primary_key=True, index=True)
+    id: uuid.UUID = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True)
     title: str = Column(String(255), index=True)
     text: str = Column(String)
     isArchived: bool = Column(Boolean, default=False)
-    author: uuid.UUID = Column(ForeignKey('user_account.id'))
+    author: uuid.UUID = Column(UUID(as_uuid=True), ForeignKey('user_account.id'))
