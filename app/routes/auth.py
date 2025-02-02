b7ef6c8611e885416df633fa1f14203fa779b319
Mauritz Marais <mauritz303@gmail.com>
2025-02-02 12:43:42 +0200

    implemented login function

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,10 +1,12 @@
 # auth route
-from fastapi import APIRouter, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from app.routes.deps import db_dependency
 from app.models.user import User
-from app.core.security import get_password_hash
+from app.core.security import get_password_hash, verify_password, create_access_token, Token
+from datetime import timedelta
 
 router = APIRouter(prefix='/users')
 
@@ -12,6 +14,7 @@ class UserCreate(BaseModel):
     username: str
     password: str
     email: str
+
 
 # user create
 @router.post('/create')
@@ -23,24 +26,50 @@ async def create_user(session: db_dependency, user_create: UserCreate):
             status_code=400,
             detail="The user with this email already exists.",
         )
+    # sanitize data?*
     # create user with hashed password
     current_user = User()
     current_user.email = user_create.email
-    current_user.password = get_password_hash(user_create.password)
+    # decode hashed password
+    current_user.password = get_password_hash(user_create.password).decode('utf8')
     current_user.username = user_create.username
 
     session.add(current_user)
     session.commit()
     session.refresh(current_user)
 
-    return {"details": f'user: {current_user}'}
+    return {"details": 'user created'}
 
 
 
 # login
 @router.post('/login')
-async def login_user():
-    ...
+async def login_user(session: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    # authenticate user
+    current_user = session.query(User).filter(User.username == form_data.username).first()
+    if not current_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    # verify password
+    correct_password = verify_password(form_data.password, current_user.password)
+    if not correct_password:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    # create access token
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": current_user.username}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
 
 
 # logout
