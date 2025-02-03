f3a15c858c4beda0df21a2f69d407070397f6609
Mauritz Marais <mauritz303@gmail.com>
2025-02-03 14:18:19 +0200

    added note crud functions

--- app/routes/notes.py
+++ app/routes/notes.py
@@ -1,9 +1,30 @@
-from fastapi import APIRouter, Request, Form, File, UploadFile
+from fastapi import APIRouter, Request, Form, File, UploadFile, Depends
 from typing import Annotated
+from pydantic import BaseModel, UUID4
+from app.models.user import User
+from app.models.note import Note
+from app.core.security import get_current_user
+from app.routes.deps import db_dependency
 
 router = APIRouter(prefix='/notes')
 
 # dependencies -> auth, db
+# rate limiter*
+
+class NoteCreate(BaseModel):
+    title: str
+    text: str
+    tags: list[str]
+
+class PublicNote(BaseModel):
+    id: UUID4
+    title: str
+    text: str
+    isArchived: bool
+    tags: list[str]
+
+class NoteDelete(BaseModel):
+    id: UUID4   
 
 # read note
 @router.post('/')
@@ -12,22 +33,60 @@ async def get_note():
 
 # create note
 @router.post('/create')
-async def create_note():
-    ...
+async def create_note(session: db_dependency, note: NoteCreate, current_user: Annotated[User, Depends(get_current_user)]):
+    try:
+        new_note = Note()
+        new_note.author = current_user.id
+        new_note.title = note.title
+        new_note.text = note.text
+        new_note.tags = note.tags.copy()
+    
+        session.add(new_note)
+        session.commit()
+        session.refresh(new_note)
+    
+    except:
+        return {"details": "An error occured"}
+
+    return {"details": "Note created"}
 
 
 # update note
 @router.put('/update')
-async def update_note():
-    ...
+async def update_note(session: db_dependency, current_note: PublicNote, current_user: Annotated[User, Depends(get_current_user)]):
+    # check if any changes?*
+    try:
+        session.query(Note).filter(Note.id == current_note.id).update({ Note.isArchived: current_note.isArchived, Note.tags: current_note.tags.copy(), Note.text: current_note.text, Note.title: current_note.title })
+        session.commit()
+    
+    except:
+        return {"details": "An error occured"}
+
+    return {"details": "Note updated"}
 
 # archive note
 @router.put('/archive')
 async def archive_note():
     ...
 
+# get all notes
+@router.get('/all', response_model=list[PublicNote])
+async def get_all_notes(session: db_dependency, current_user: Annotated[User, Depends(get_current_user)]):
+    all_notes = session.query(Note).filter(Note.author == current_user.id).all()
+
+    return all_notes
+
+
+# get all archived notes
 
 # delete note
 @router.delete('/delete')
-async def delete_note():
-    ...
+async def delete_note(session: db_dependency, current_note: NoteDelete, current_user: Annotated[User, Depends(get_current_user)]):
+    try:
+        session.query(Note).filter(Note.id == current_note.id).delete()
+        session.commit()
+
+    except:
+        return {"details": "An error occured"}    
+    
+    return {"details": "note deleted"}
