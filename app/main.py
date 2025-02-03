b443aa6aa1e05f9c659430287c94a55516305af2
Mauritz Marais <mauritz303@gmail.com>
2025-02-03 14:14:23 +0200

    added cors origins

--- app/main.py
+++ app/main.py
@@ -1,28 +1,30 @@
 from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
-from fastapi.security import OAuth2PasswordBearer
 from app.routes import notes
 from app.routes import auth
 from typing import Annotated
 import app.models.user as users
-from app.core.config import engine, SessionLocal
-from sqlalchemy.orm import Session
+from app.core.config import engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 users.Base.metadata.create_all(bind=engine)
 
+origins = ['http://localhost']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(notes.router)
 app.include_router(auth.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
-
 
 
 
