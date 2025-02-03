0330111f8f57d9d9b09fd5cecbe3f4c74c412a86
Mauritz Marais <mauritz303@gmail.com>
2025-02-03 14:17:53 +0200

    added font and color update functions

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,7 +5,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from app.routes.deps import db_dependency
 from app.models.user import User
-from app.core.security import get_password_hash, verify_password, create_access_token, Token
+from app.core.security import get_password_hash, verify_password, create_access_token, Token, get_current_user
 from datetime import timedelta
 
 router = APIRouter(prefix='/users')
@@ -14,6 +14,12 @@ class UserCreate(BaseModel):
     username: str
     password: str
     email: str
+
+class UserFont(BaseModel):
+    font_theme: str
+
+class UserColor(BaseModel):
+    color_theme: str
 
 
 # user create
@@ -44,7 +50,7 @@ async def create_user(session: db_dependency, user_create: UserCreate):
 
 # login
 @router.post('/login')
-async def login_user(session: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login_user(session: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
     # authenticate user
     current_user = session.query(User).filter(User.username == form_data.username).first()
     if not current_user:
@@ -66,13 +72,46 @@ async def login_user(session: db_dependency, form_data: Annotated[OAuth2Password
     # create access token
     access_token_expires = timedelta(minutes=30)
     access_token = create_access_token(
-        data={"sub": current_user.username}, expires_delta=access_token_expires
+        data={"sub": current_user.username, "color_theme": current_user.color_theme, "font_theme": current_user.font_theme}, expires_delta=access_token_expires
     )
 
     return Token(access_token=access_token, token_type="bearer")
 
 
-# logout
-@router.post('/logout')
-async def logout_user():
-    ...
+# update color_theme / font_theme
+@router.post('/color')
+async def update_color_theme(session: db_dependency, theme: UserColor, current_user: Annotated[User, Depends(get_current_user)]):
+   
+    allowed_values = ['light', 'dark', 'system']
+
+    if theme.color_theme not in allowed_values:
+        raise HTTPException(
+            status_code=400,
+            detail="incorrect value"
+        )
+        
+    session.query(User).filter(User.id == current_user.id).update({User.color_theme: theme.color_theme.lower()})
+    session.commit()
+
+    return {"details": "color theme updated"}
+     
+
+@router.post('/font')
+async def update_font_theme(session: db_dependency, theme: UserFont, current_user: Annotated[User, Depends(get_current_user)]):
+    
+    allowed_values = ['serif', 'san-serif', 'monospace']
+
+    if theme.font_theme not in allowed_values:
+        raise HTTPException(
+            status_code=400,
+            detail="incorrect value"
+        )
+        
+    session.query(User).filter(User.id == current_user.id).update({User.font_theme: theme.font_theme})
+    session.commit()
+
+    return {"details": "font theme updated"}
+
+# password reset*
+
+# google auth*
